5ef6cd49cb440bc3e27ee77f1596f55a39f49d99
asrafulhoque25 <asraful15-6625@diu.edu.bd>
2023-12-26 19:07:34 +0600

    page added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,3 @@
-/**
- * @Script js for (Template/Project Name)
- *
- * @project     - Project Name
- * @author      -
- * @created_by  - Asraful Hoque
- * @created_at  -
- * @modified_by -
- */
-
-/**
- * ========================================================
- * this function execute when window properly loaded
- * ===========================================================
- */
 
 
 
@@ -59,52 +44,6 @@ document.addEventListener("DOMContentLoaded", function() {
  * this function execute when DOM element ready
  * ===========================================================
  */
-
-$(document).ready(function () {
-    // testimonial-active
-    $(function () {
-        if ($(".testimonials-active").length) {
-            $(".testimonials-active").owlCarousel({
-                items: 3,
-                margin: 30,
-                nav: true,
-                loop: true,
-                autoplay: true,
-                autoplayTimeout: 2500,
-                animateOut: "fadeOut",
-                smartSpeed: 2500,
-                navText: [
-                    "<img src='assets/img/arrow-left.svg' class='img-fluid' />",
-                    "<img src='assets/img/arrow-right-s.svg' class='img-fluid' />",
-                ],
-                dots: false,
-                autoplayHoverPause: true,
-                responsive: {
-                    0: {
-                        items: 1,
-                    },
-                    767: {
-                        items: 2,
-                    },
-                    1200: {
-                        items: 3,
-                    },
-                },
-            });
-        }
-    });
-
-
-
-
-    //shape animation
-
-
-    
-
-
-});
-
 
     // tooltip animation
     var scene = document.getElementById('js-scene');
