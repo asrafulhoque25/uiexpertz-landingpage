76a46d9e7baafbab85cde37c9af3dade2382087f
Ashik <ashikur1993@gmail.com>
2023-12-27 18:46:20 +0600

    Testimonial slider Desktop version complete

---
+++ assets/js/dev-slider.js
@@ -0,0 +1,9 @@
+new Splide( '#testimonialSlider', {
+    type   : 'loop',
+    perPage: 1,
+    perMove: 1,
+    autoplay: true,
+    arrows: true,
+    pagination: false,
+    gap: '2rem',
+} ).mount();
